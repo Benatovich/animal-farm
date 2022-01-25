7a60d3c71d3e31a43a71950978a087941d1b4aef
Benatovich <sambenatovich@gmail.com>
2022-01-25 18:44:11 -0500

    I THINK my files/folders are set up properly. started working in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Main from './Main/Main.js';
+import { animals } from './data.js';
+
 
 function App() {
   return (
