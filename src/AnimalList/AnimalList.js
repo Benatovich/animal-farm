eee6180c43bc1b2b685d7278607874f5cf687c98
Benatovich <sambenatovich@gmail.com>
2022-01-25 21:05:44 -0500

    still need to tinker with the style to make the animals appear in the right places

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -2,13 +2,13 @@ import './AnimalList.css';
 
 export default function AnimalList(props) {
     // cool zone
-  console.log(props.animals);
+//   console.log(props.animals);
   return (
     <div className='animal-list'>
       {
         //   props.animals or props.animals.animals?
         props.animals.map((item, i) => (
-          <div className='animal' key={item.name + i} style={{ left: `${item.left}`, top: `${item.top}` }}>
+          <div className='animal' key={item.name + i} style={{ left: `${item.left + i}`, top: `${item.top + i}` }}>
             <img alt={item.name} src={`/animals/${item.type}.svg`} />
             <h3>{item.name}</h3>
             <span>{item.says}</span>
