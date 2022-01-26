65fe96fbd6bb50bb6820e29fb0fde91e89329cd5
Benatovich <sambenatovich@gmail.com>
2022-01-25 19:20:41 -0500

    errors are gone now that i have some code in main.js, but background isn't rendering. header and footer appear to work properly

---
+++ src/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import backgroundImg from '../background.png';
+import './Main.css';
+
+export default function Main(props) {
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg}.png)` }}>
+
+    </main>
+  );
+}
