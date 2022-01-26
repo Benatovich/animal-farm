2b94ba84a97e93fe0d326c59af19f1774456e3e5
Benatovich <sambenatovich@gmail.com>
2022-01-25 20:02:58 -0500

    i broke everything

--- src/AnimalList/AnimalList.js
+++ src/AnimalList/AnimalList.js
@@ -1,9 +1,29 @@
 import './AnimalList.css';
 
 export default function AnimalList(props) {
+    // cool zone
   return (
     <div className='animal-list'>
-            
+      {
+        props.animals
+      }
     </div>
   );
 }
+
+// comparison code below
+/* {zodiac.map((item) => (
+        <ZodiacCard key={item.name} {...item} /> */
+
+
+// export default function Main(props) {
+//   // cool zone
+//   return (
+//     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+//       {
+//         props.animals.map((animal, i) =>
+//           <AnimalList key={`${animal.name} + ${i}`} name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />)
+//       }
+//     </main>
+//   );
+// }
